9c016350ddc2bbb07c18f4a2be5e02d03eb981ad
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-30 23:09:06 -0300

    creation of entities

---
+++ src/entities/product.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity } from "typeorm";
+import { BaseEntity } from "./base.entity";
+
+@Entity()
+export class Product extends BaseEntity {
+  @Column()
+  name: string
+  
+  @Column()
+  price: number;
+};
