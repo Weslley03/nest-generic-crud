b744768154763f5560b2291de690065af7626897
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-30 23:10:37 -0300

    add controller for route configuration

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Product } from "src/entities/product.entity";
+import { ProductService } from "src/services/product.service";
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {};
+
+  @Get()
+  findAll() {
+    return this.productService.findAll();
+  };
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.productService.findOne(id);
+  };
+
+  @Post()
+  create(@Body() product: Product) {
+    return this.productService.create(product);
+  };
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() product: Product) {
+    return this.productService.update(id, product);
+  };
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productService.delete(id);
+  };
+};
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { User } from "src/entities/user.entity";
+import { UserService } from "src/services/user.service";
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {};
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  };
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.userService.findOne(id);
+  };
+
+  @Post()
+  create(@Body() user: User) {
+    return this.userService.create(user);
+  };
+
+  @Put(':id')
+  update(
+    @Param('id')
+    id: number,
+    @Body()
+    user: User
+  ) {
+    return this.userService.update(id, user);
+  };
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.userService.delete(id);
+  };
+};
