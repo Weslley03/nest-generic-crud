56ed2f9831834a1f725a197e71d0637f3f01ecf2
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-31 16:29:40 -0300

    adjusted `user.service` to use `base.service`

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,30 +3,13 @@ import { Injectable } from "@nestjs/common";
 import { DeepPartial } from "typeorm";
 import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
 import { UserRepository } from "src/repositories/user.repository";
+import { BaseService } from "./base.service";
 
 @Injectable()
-export class UserService {
+export class UserService extends BaseService<User>{
   constructor(
     private readonly userRepository: UserRepository
-  ) {};
-
-  async findAll(): Promise<User[]> {
-    return this.userRepository.findAll();
-  };
-
-  async findOne(id: number): Promise<User | null> {
-    return this.userRepository.findOneById(id);
-  };
-
-  async create(userData: DeepPartial<User>): Promise<User> {
-    return await this.userRepository.createEntity(userData);
-  };
-
-  async update(id: number, userData: QueryDeepPartialEntity<User>): Promise<User | null> {
-    return await this.userRepository.updateEntity(id, userData);
-  };
-
-  async delete(id: number): Promise<void> {
-    return await this.userRepository.deleteEntity(id);
+  ) {
+    super(userRepository);
   };
 };
